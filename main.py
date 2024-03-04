f4b51221350ac9aad7ec9f5db9dcfec87f23bfc8
ROMUSKING <romusking@gmail.com>
2024-03-04 21:43:20 +0000

    added OpenAI and Gemini APIs to get them talking in a bit

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-import os
-import pathlib
-import textwrap
-
+import keys
 import google.generativeai as genai
+from openai import OpenAI
 
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=keys.API_CHATGPT)
+
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
+
+print(chat_completion.choices[0].message.content)
 # Used to securely store your API key
-genai.configure(api_key=os.environ["API_KEY"])
+genai.configure(api_key=keys.API_GEMINI)
 
-model = genai.GenerativeModel('gemini')
+model = genai.GenerativeModel('gemini-pro')
+response = model.generate_content("what should i make as my first independent project from boot.dev?")
+print(response.text)  # cold.
